98c12b7b0b43a3b516f32e19928786fa106ce1f6
Joe <gs205642@gmail.com>
2025-08-14 15:34:33 +0800

    fix: secrets换行

--- main.go
+++ main.go
@@ -97,6 +97,7 @@ func getChaoHuaList(sub string) (ids []*chaohua, err error) {
 	}
 	client := resty.New()
 	client.SetHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36").
+		SetHeader("Referer", "https://weibo.com").
 		SetCookie(&http.Cookie{Name: "SUB", Value: sub}).
 		SetQueryParam("tabid", "231093_-_chaohua")
 
